a81544ba7832da0818fcaaca8f5ef7ccc86de80e
jckim3 <jckim3@gmail.com>
2022-12-06 22:34:43 -0700

    7 .테스트 클래스 생성, 테스트

--- ArticleApp.Models.Test/ArticleRepositoryTest.cs
+++ ArticleApp.Models.Test/ArticleRepositoryTest.cs
@@ -1,8 +1,10 @@
 ﻿// C:\Users\jckim\.nuget\packages\microsoft.entityframeworkcore.inmemory\7.0.0\ 설치
-
+// C:\Users\jckim\.nuget\packages\microsoft.entityframeworkcore.sqlserver\7.0.0\ 설치
 using ArticleApp.Models.Articles;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Reflection.Metadata;
 
 namespace ArticleApp.Models.Test
 {
@@ -24,12 +26,70 @@ namespace ArticleApp.Models.Test
                 var model = new Article { Title = "Initiate Aritlce" ,Created = DateTime.Now};
                 await repository.AddArticleAsync(model);
                 await context.SaveChangesAsync();
+
+                // Add second record
+                model = new Article { Title = "2nd Aritlce", Created = DateTime.Now };
+                await repository.AddArticleAsync(model);
+                await context.SaveChangesAsync();
             }
 
             using (var context = new ArticleAppDBContext(options))
             {
-                Assert.AreEqual(1, await context.Articles.CountAsync());
+                Assert.AreEqual(2, await context.Articles.CountAsync());
+                var model = await context.Articles.Where(m=>m.Id==1).SingleOrDefaultAsync();
+                Assert.AreEqual("Initiate Aritlce", model?.Title);
             }
+
+            // GetAllSync() method Test
+            using (var context = new ArticleAppDBContext(options))
+            {
+                var repository = new ArticleRepository(context);
+                var model = new Article { Title = "3rd Aritlce", Created = DateTime.Now };
+                await repository.AddArticleAsync(model);
+                await context.SaveChangesAsync();
+            }
+            // 3개 들어가 있는거 구나. 
+            using (var context = new ArticleAppDBContext(options))
+            {
+                var repository = new ArticleRepository(context);
+                var models = await repository.GetArticlesAsync();
+                Assert.AreEqual(3, models.Count);
+            }
+
+            // GetByID
+            using (var context = new ArticleAppDBContext(options))
+            {
+                var repository = new ArticleRepository(context);
+                var model = await repository.GetArticleByIdAsync(2);
+                Assert.IsTrue(model?.Title.Contains("2nd"));
+            }
+
+            // Edit 
+            using (var context = new ArticleAppDBContext(options))
+            {
+                var repository = new ArticleRepository(context);
+
+                var model = await repository.GetArticleByIdAsync(2);
+                model.Title = "2nd Busy Article";
+                await repository.EditArticleAsync(model);
+                await context.SaveChangesAsync();
+
+                var model2 = await repository.GetArticleByIdAsync(2);
+                Assert.IsTrue(model2?.Title.Contains("Busy"));
+            }
+
+            // Delete
+            using (var context = new ArticleAppDBContext(options))
+            {
+                var repository = new ArticleRepository(context);
+                await repository.DeleteArticleAsync(2);
+                await context.SaveChangesAsync();
+
+                var models = await repository.GetArticlesAsync();
+                Assert.AreEqual(2, models.Count);
+
+            }
+
         }
     }
 }
